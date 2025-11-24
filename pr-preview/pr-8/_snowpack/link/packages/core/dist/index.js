28b9ca53b87ce8a66850597e51e0647e4893cc9a
noraeb <noraeb@users.noreply.github.com>
2025-11-24 11:42:22 +0000

    Deploy preview for PR 8 🛫

--- pr-preview/pr-8/_snowpack/link/packages/core/dist/index.js
+++ pr-preview/pr-8/_snowpack/link/packages/core/dist/index.js
@@ -1,6 +1,6 @@
 export { newOpenEvent } from './foundation.js';
 export { newEditEvent, isComplex, isInsert, isNamespaced, isUpdate, isRemove, } from './foundation.js';
-export { isEditV2, isRemoveV2, isInsertV2, isComplexV2, isSetAttributesV2, isSetTextContentV2 } from './foundation.js';
+export { isEditV2, isRemoveV2, isInsertV2, isComplexV2, isSetAttributesV2, isSetTextContentV2, } from './foundation.js';
 export { newEditEventV2 } from './foundation.js';
 export { handleEditV2 } from './foundation.js';
 export { cyrb64 } from './foundation.js';
@@ -8,15 +8,14 @@ export { newEditCompletedEvent } from './foundation.js';
 export { crossProduct } from './foundation.js';
 export { XMLEditor } from './foundation.js';
 // Deprecated open-event
-export { newOpenDocEvent } from './foundation/deprecated/open-event.js';
+export { newOpenDocEvent, } from './foundation/deprecated/open-event.js';
 // Deprecated waiter
 export { newPendingStateEvent, } from './foundation/deprecated/waiter.js';
 // Deprecated history
 export { newLogEvent, newIssueEvent, } from './foundation/deprecated/history.js';
 // Deprecated validation
 export { newValidateEvent } from './foundation/deprecated/validation.js';
-// Deprecated editor (aliased to avoid conflicts)
-export { newActionEvent, isCreate, isDelete, isReplace, isSimple, isMove, isUpdate as isDeprecatedUpdate, createUpdateAction, } from './foundation/deprecated/editor.js';
+export { newActionEvent, isCreate, isDelete, isReplace, isSimple, isMove, isUpdate as isEditorUpdate, createUpdateAction, } from './foundation/deprecated/editor.js';
 // Settings
 export { newSettingsUIEvent, newLoadNsdocEvent, } from './foundation/deprecated/settings.js';
 // API
