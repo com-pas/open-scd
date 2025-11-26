ecec576fa8923b9bd2446f855a423f35664213ed
Nora Blomaard <n.blomaard@gmail.com>
2025-11-26 08:29:33 +0100

    fix: update history to match compas

--- packages/core/foundation/deprecated/history.ts
+++ packages/core/foundation/deprecated/history.ts
@@ -1,5 +1,3 @@
-import { EditV2 } from '../edit.js';
-
 type InfoEntryKind = 'info' | 'warning' | 'error';
 
 export type LogEntryType = 'info' | 'warning' | 'error' | 'action' | 'reset';
@@ -12,9 +10,6 @@ export interface LogDetailBase {
 /** The [[`LogEntry`]] for a committed [[`EditorAction`]]. */
 export interface CommitDetail extends LogDetailBase {
   kind: 'action';
-  redo: EditV2;
-  undo: EditV2;
-  squash?: boolean;
 }
 /** A [[`LogEntry`]] for notifying the user. */
 export interface InfoDetail extends LogDetailBase {
