3da77d9716ea8feeefd5d497bc50f0175e966651
noraeb <noraeb@users.noreply.github.com>
2025-11-26 12:45:50 +0000

    Remove preview for PR 8 🛬

--- pr-preview/pr-8/openscd/dist/core/foundation/deprecated/open-event.js
+++
@@ -1,9 +0,0 @@
-export function newOpenDocEvent(doc, docName, eventInitDict) {
-    return new CustomEvent('open-doc', {
-        bubbles: true,
-        composed: true,
-        ...eventInitDict,
-        detail: { doc, docName, ...eventInitDict?.detail },
-    });
-}
-//# sourceMappingURL=open-event.js.map
