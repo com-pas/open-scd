c63b9c83b9fad9ff5eca8cc5fea5d1d515242890
noraeb <noraeb@users.noreply.github.com>
2026-01-23 09:54:28 +0000

    Deploy preview for PR 20 🛫

---
+++ pr-preview/pr-20/openscd/dist/index.js
@@ -0,0 +1,7 @@
+// NS Doc
+export { initializeNsdoc } from './foundation/nsdoc.js';
+export { newConfigurePluginEvent } from './plugin.events.js';
+export { pluginTag } from './plugin-tag.js';
+// Schema
+export { SCL_NAMESPACE } from './schemas.js';
+//# sourceMappingURL=index.js.map
