08fe49f0ca769290c233f138e4ad648f35f240b5
clepski <clepski@users.noreply.github.com>
2026-02-02 14:55:15 +0000

    Deploy preview for PR 28 🛫

---
+++ pr-preview/pr-28/_snowpack/link/packages/core/dist/index.js
@@ -0,0 +1,23 @@
+export { newOpenEvent } from './foundation.js';
+export { newEditEvent, isComplex, isInsert, isNamespaced, isUpdate, isRemove, } from './foundation.js';
+export { isEditV2, isRemoveV2, isInsertV2, isComplexV2, isSetAttributesV2, isSetTextContentV2, } from './foundation.js';
+export { newEditEventV2 } from './foundation.js';
+export { handleEditV2 } from './foundation.js';
+export { cyrb64 } from './foundation.js';
+export { newEditCompletedEvent } from './foundation.js';
+export { crossProduct } from './foundation.js';
+export { XMLEditor } from './foundation.js';
+// Deprecated open-event
+export { newOpenDocEvent, } from './foundation/deprecated/open-event.js';
+// Deprecated waiter
+export { newPendingStateEvent, } from './foundation/deprecated/waiter.js';
+// Deprecated history
+export { newLogEvent, newIssueEvent, } from './foundation/deprecated/history.js';
+// Deprecated validation
+export { newValidateEvent } from './foundation/deprecated/validation.js';
+export { newActionEvent, isCreate, isDelete, isReplace, isSimple, isMove, isUpdate as isEditorUpdate, createUpdateAction, invert, } from './foundation/deprecated/editor.js';
+// Settings
+export { newSettingsUIEvent, newLoadNsdocEvent, } from './foundation/deprecated/settings.js';
+// API
+export { OscdApi } from './api/api.js';
+//# sourceMappingURL=index.js.map
---
+++ pr-preview/pr-28/openscd/dist/open-scd.component.js
@@ -0,0 +1,12 @@
+import { __decorate } from "../../_snowpack/pkg/tslib.js";
+import { customElement } from '../../_snowpack/pkg/lit-element.js';
+import { OpenSCD } from './open-scd.js';
+/** The `<open-scd>` custom element is the main entry point of the
+ * Open Substation Configuration Designer. */
+let OpenSCDComponent = class OpenSCDComponent extends OpenSCD {
+};
+OpenSCDComponent = __decorate([
+    customElement('open-scd')
+], OpenSCDComponent);
+export { OpenSCDComponent };
+//# sourceMappingURL=open-scd.component.js.map
