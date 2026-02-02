409ea6bae2469f58e739b405d18e1adab51a3501
clepski <clepski@users.noreply.github.com>
2026-02-02 14:50:08 +0000

    Remove preview for PR 27 🛬

--- pr-preview/pr-27/openscd/dist/index.js
+++
@@ -1,7 +0,0 @@
-// NS Doc
-export { initializeNsdoc } from './foundation/nsdoc.js';
-export { newConfigurePluginEvent } from './plugin.events.js';
-export { pluginTag } from './plugin-tag.js';
-// Schema
-export { SCL_NAMESPACE } from './schemas.js';
-//# sourceMappingURL=index.js.map
