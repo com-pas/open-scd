af5854584e65bab2a17e452acda7c4d4ca9800ff
clepski <clepski@users.noreply.github.com>
2026-03-31 12:29:44 +0000

    Deploy preview for PR 35 🛫

---
+++ pr-preview/pr-35/_snowpack/pkg/@omicronenergy/oscd-ui/action-pane/oscd-action-pane.js
@@ -0,0 +1,243 @@
+import { e as __decorate } from '../../../common/tslib.es6-887c7ab2.js';
+import { s as s$1, i as i$2 } from '../../../common/lit-element-7082a332.js';
+import { h as html, n as nothing } from '../../../common/lit-html-487c1dfc.js';
+import { E } from '../../../common/lit-html-4dfc8767.js';
+import { S as ScopedElementsMixin } from '../../../common/lit-element-ce8266a9.js';
+import { O as OscdIcon } from '../../../common/OscdIcon-753f0e38.js';
+import '../../../common/render-d4c46927.js';
+
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const t=globalThis,e=t.ShadowRoot&&(void 0===t.ShadyCSS||t.ShadyCSS.nativeShadow)&&"adoptedStyleSheets"in Document.prototype&&"replace"in CSSStyleSheet.prototype,s=Symbol(),o=new WeakMap;class n{constructor(t,e,o){if(this._$cssResult$=!0,o!==s)throw Error("CSSResult is not constructable. Use `unsafeCSS` or `css` instead.");this.cssText=t,this.t=e;}get styleSheet(){let t=this.o;const s=this.t;if(e&&void 0===t){const e=void 0!==s&&1===s.length;e&&(t=o.get(s)),void 0===t&&((this.o=t=new CSSStyleSheet).replaceSync(this.cssText),e&&o.set(s,t));}return t}toString(){return this.cssText}}const r=t=>new n("string"==typeof t?t:t+"",void 0,s),S=(s,o)=>{if(e)s.adoptedStyleSheets=o.map(t=>t instanceof CSSStyleSheet?t:t.styleSheet);else for(const e of o){const o=document.createElement("style"),n=t.litNonce;void 0!==n&&o.setAttribute("nonce",n),o.textContent=e.cssText,s.appendChild(o);}},c=e?t=>t:t=>t instanceof CSSStyleSheet?(t=>{let e="";for(const s of t.cssRules)e+=s.cssText;return r(e)})(t):t;
+
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const{is:i,defineProperty:e$1,getOwnPropertyDescriptor:h,getOwnPropertyNames:r$1,getOwnPropertySymbols:o$1,getPrototypeOf:n$1}=Object,a=globalThis,c$1=a.trustedTypes,l=c$1?c$1.emptyScript:"",p=a.reactiveElementPolyfillSupport,d=(t,s)=>t,u={toAttribute(t,s){switch(s){case Boolean:t=t?l:null;break;case Object:case Array:t=null==t?t:JSON.stringify(t);}return t},fromAttribute(t,s){let i=t;switch(s){case Boolean:i=null!==t;break;case Number:i=null===t?null:Number(t);break;case Object:case Array:try{i=JSON.parse(t);}catch(t){i=null;}}return i}},f=(t,s)=>!i(t,s),b={attribute:!0,type:String,converter:u,reflect:!1,useDefault:!1,hasChanged:f};Symbol.metadata??=Symbol("metadata"),a.litPropertyMetadata??=new WeakMap;class y extends HTMLElement{static addInitializer(t){this._$Ei(),(this.l??=[]).push(t);}static get observedAttributes(){return this.finalize(),this._$Eh&&[...this._$Eh.keys()]}static createProperty(t,s=b){if(s.state&&(s.attribute=!1),this._$Ei(),this.prototype.hasOwnProperty(t)&&((s=Object.create(s)).wrapped=!0),this.elementProperties.set(t,s),!s.noAccessor){const i=Symbol(),h=this.getPropertyDescriptor(t,i,s);void 0!==h&&e$1(this.prototype,t,h);}}static getPropertyDescriptor(t,s,i){const{get:e,set:r}=h(this.prototype,t)??{get(){return this[s]},set(t){this[s]=t;}};return {get:e,set(s){const h=e?.call(this);r?.call(this,s),this.requestUpdate(t,h,i);},configurable:!0,enumerable:!0}}static getPropertyOptions(t){return this.elementProperties.get(t)??b}static _$Ei(){if(this.hasOwnProperty(d("elementProperties")))return;const t=n$1(this);t.finalize(),void 0!==t.l&&(this.l=[...t.l]),this.elementProperties=new Map(t.elementProperties);}static finalize(){if(this.hasOwnProperty(d("finalized")))return;if(this.finalized=!0,this._$Ei(),this.hasOwnProperty(d("properties"))){const t=this.properties,s=[...r$1(t),...o$1(t)];for(const i of s)this.createProperty(i,t[i]);}const t=this[Symbol.metadata];if(null!==t){const s=litPropertyMetadata.get(t);if(void 0!==s)for(const[t,i]of s)this.elementProperties.set(t,i);}this._$Eh=new Map;for(const[t,s]of this.elementProperties){const i=this._$Eu(t,s);void 0!==i&&this._$Eh.set(i,t);}this.elementStyles=this.finalizeStyles(this.styles);}static finalizeStyles(s){const i=[];if(Array.isArray(s)){const e=new Set(s.flat(1/0).reverse());for(const s of e)i.unshift(c(s));}else void 0!==s&&i.push(c(s));return i}static _$Eu(t,s){const i=s.attribute;return !1===i?void 0:"string"==typeof i?i:"string"==typeof t?t.toLowerCase():void 0}constructor(){super(),this._$Ep=void 0,this.isUpdatePending=!1,this.hasUpdated=!1,this._$Em=null,this._$Ev();}_$Ev(){this._$ES=new Promise(t=>this.enableUpdating=t),this._$AL=new Map,this._$E_(),this.requestUpdate(),this.constructor.l?.forEach(t=>t(this));}addController(t){(this._$EO??=new Set).add(t),void 0!==this.renderRoot&&this.isConnected&&t.hostConnected?.();}removeController(t){this._$EO?.delete(t);}_$E_(){const t=new Map,s=this.constructor.elementProperties;for(const i of s.keys())this.hasOwnProperty(i)&&(t.set(i,this[i]),delete this[i]);t.size>0&&(this._$Ep=t);}createRenderRoot(){const t=this.shadowRoot??this.attachShadow(this.constructor.shadowRootOptions);return S(t,this.constructor.elementStyles),t}connectedCallback(){this.renderRoot??=this.createRenderRoot(),this.enableUpdating(!0),this._$EO?.forEach(t=>t.hostConnected?.());}enableUpdating(t){}disconnectedCallback(){this._$EO?.forEach(t=>t.hostDisconnected?.());}attributeChangedCallback(t,s,i){this._$AK(t,i);}_$ET(t,s){const i=this.constructor.elementProperties.get(t),e=this.constructor._$Eu(t,i);if(void 0!==e&&!0===i.reflect){const h=(void 0!==i.converter?.toAttribute?i.converter:u).toAttribute(s,i.type);this._$Em=t,null==h?this.removeAttribute(e):this.setAttribute(e,h),this._$Em=null;}}_$AK(t,s){const i=this.constructor,e=i._$Eh.get(t);if(void 0!==e&&this._$Em!==e){const t=i.getPropertyOptions(e),h="function"==typeof t.converter?{fromAttribute:t.converter}:void 0!==t.converter?.fromAttribute?t.converter:u;this._$Em=e;const r=h.fromAttribute(s,t.type);this[e]=r??this._$Ej?.get(e)??r,this._$Em=null;}}requestUpdate(t,s,i,e=!1,h){if(void 0!==t){const r=this.constructor;if(!1===e&&(h=this[t]),i??=r.getPropertyOptions(t),!((i.hasChanged??f)(h,s)||i.useDefault&&i.reflect&&h===this._$Ej?.get(t)&&!this.hasAttribute(r._$Eu(t,i))))return;this.C(t,s,i);}!1===this.isUpdatePending&&(this._$ES=this._$EP());}C(t,s,{useDefault:i,reflect:e,wrapped:h},r){i&&!(this._$Ej??=new Map).has(t)&&(this._$Ej.set(t,r??s??this[t]),!0!==h||void 0!==r)||(this._$AL.has(t)||(this.hasUpdated||i||(s=void 0),this._$AL.set(t,s)),!0===e&&this._$Em!==t&&(this._$Eq??=new Set).add(t));}async _$EP(){this.isUpdatePending=!0;try{await this._$ES;}catch(t){Promise.reject(t);}const t=this.scheduleUpdate();return null!=t&&await t,!this.isUpdatePending}scheduleUpdate(){return this.performUpdate()}performUpdate(){if(!this.isUpdatePending)return;if(!this.hasUpdated){if(this.renderRoot??=this.createRenderRoot(),this._$Ep){for(const[t,s]of this._$Ep)this[t]=s;this._$Ep=void 0;}const t=this.constructor.elementProperties;if(t.size>0)for(const[s,i]of t){const{wrapped:t}=i,e=this[s];!0!==t||this._$AL.has(s)||void 0===e||this.C(s,void 0,i,e);}}let t=!1;const s=this._$AL;try{t=this.shouldUpdate(s),t?(this.willUpdate(s),this._$EO?.forEach(t=>t.hostUpdate?.()),this.update(s)):this._$EM();}catch(s){throw t=!1,this._$EM(),s}t&&this._$AE(s);}willUpdate(t){}_$AE(t){this._$EO?.forEach(t=>t.hostUpdated?.()),this.hasUpdated||(this.hasUpdated=!0,this.firstUpdated(t)),this.updated(t);}_$EM(){this._$AL=new Map,this.isUpdatePending=!1;}get updateComplete(){return this.getUpdateComplete()}getUpdateComplete(){return this._$ES}shouldUpdate(t){return !0}update(t){this._$Eq&&=this._$Eq.forEach(t=>this._$ET(t,this[t])),this._$EM();}updated(t){}firstUpdated(t){}}y.elementStyles=[],y.shadowRootOptions={mode:"open"},y[d("elementProperties")]=new Map,y[d("finalized")]=new Map,p?.({ReactiveElement:y}),(a.reactiveElementVersions??=[]).push("2.1.2");
+
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const o$2={attribute:!0,type:String,converter:u,reflect:!1,hasChanged:f},r$2=(t=o$2,e,r)=>{const{kind:n,metadata:i}=r;let s=globalThis.litPropertyMetadata.get(i);if(void 0===s&&globalThis.litPropertyMetadata.set(i,s=new Map),"setter"===n&&((t=Object.create(t)).wrapped=!0),s.set(r.name,t),"accessor"===n){const{name:o}=r;return {set(r){const n=e.get.call(this);e.set.call(this,r),this.requestUpdate(o,n,t,!0,r);},init(e){return void 0!==e&&this.C(o,void 0,t,e),e}}}if("setter"===n){const{name:o}=r;return function(r){const n=this[o];e.call(this,r),this.requestUpdate(o,n,t,!0,r);}}throw Error("Unsupported decorator location: "+n)};function n$2(t){return (e,o)=>"object"==typeof o?r$2(t,e,o):((t,e,o)=>{const r=e.hasOwnProperty(o);return e.constructor.createProperty(o,t),r?Object.getOwnPropertyDescriptor(e,o):void 0})(t,e,o)}
+
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const t$1={ATTRIBUTE:1,CHILD:2,PROPERTY:3,BOOLEAN_ATTRIBUTE:4,EVENT:5,ELEMENT:6},e$2=t=>(...e)=>({_$litDirective$:t,values:e});class i$1{constructor(t){}get _$AU(){return this._$AM._$AU}_$AT(t,e,i){this._$Ct=t,this._$AM=e,this._$Ci=i;}_$AS(t,e){return this.update(t,e)}update(t,e){return this.render(...e)}}
+
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const e$3=e$2(class extends i$1{constructor(t){if(super(t),t.type!==t$1.ATTRIBUTE||"class"!==t.name||t.strings?.length>2)throw Error("`classMap()` can only be used in the `class` attribute and must be the only part in the attribute.")}render(t){return " "+Object.keys(t).filter(s=>t[s]).join(" ")+" "}update(s,[i]){if(void 0===this.st){this.st=new Set,void 0!==s.strings&&(this.nt=new Set(s.strings.join(" ").split(/\s/).filter(t=>""!==t)));for(const t in i)i[t]&&!this.nt?.has(t)&&this.st.add(t);return this.render(i)}const r=s.element.classList;for(const t of this.st)t in i||(r.remove(t),this.st.delete(t));for(const t in i){const s=!!i[t];s===this.st.has(t)||this.nt?.has(t)||(s?(r.add(t),this.st.add(t)):(r.remove(t),this.st.delete(t)));}return E}});
+
+function closestTo(node, selector) {
+    const closest = node.nodeType === Node.ELEMENT_NODE
+        ? node.closest(selector)
+        : null;
+    if (closest) {
+        return closest;
+    }
+    const root = node.getRootNode();
+    if (root instanceof ShadowRoot) {
+        return closestTo(root.host, selector);
+    }
+    return null;
+}
+/**
+ * @tag oscd-action-pane
+ * @slot action - Places element in <nav/> section.
+ * @slot icon - Action Pane Icon.
+ * @slot - The default slot will be rendered into the pane body in a single column.
+ * @cssprop [--oscd-action-pane-theme-primary=var(--md-sys-color-primary)] - Color for border on even levels.
+ * @cssprop [--oscd-action-pane-theme-on-primary=var(--md-sys-color-on)-primary] - Pane color for the uneven levels.
+ * @cssprop [--oscd-action-pane-theme-secondary=var(--md-sys-color-secondary)] - Color for border on uneven levels.
+ * @cssprop [--oscd-action-pane-theme-surface=var(--md-sys-color-surface)] - Pane color for the even levels.
+ * @cssprop [--oscd-action-pane-theme-on-surface=var(--md-sys-color-on-surface)] - Icon and label color.
+ * @cssprop [--oscd-action-pane-theme-font=var(--md-sys-color-font)] - Font for label.
+ *
+ * @summary A responsive container rendering actions in a header.
+ * @tag oscd-action-pane
+ */
+class OscdActionPane extends ScopedElementsMixin(s$1) {
+    constructor() {
+        super(...arguments);
+        /** color header with secondary theme color while focus is within */
+        this.secondary = false;
+        /** highlight pane with dotted outline */
+        this.highlighted = false;
+        /** nesting level, default (closest pane ancestor's level) + 1 */
+        this.level = 1;
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        this.tabIndex = 0;
+        this.parentPane =
+            closestTo(this.parentNode, 'oscd-action-pane') ??
+                undefined;
+    }
+    get resolvedLevel() {
+        const base = this.parentPane
+            ? this.parentPane.resolvedLevel + 1
+            : this.level;
+        return Math.floor(base);
+    }
+    renderHeader() {
+        const content = html `<span
+        ><slot name="icon"
+          >${this.icon
+            ? html `<oscd-icon>${this.icon}</oscd-icon>`
+            : nothing}</slot
+        ></span
+      >
+      ${this.label ?? nothing}
+      <nav>
+        <slot name="action"></slot>
+      </nav>`;
+        const headingLevel = Math.floor(Math.max(this.resolvedLevel, 1));
+        // Sometimes a TemplateResult is passed in as Label, not a string. So only when it's a string show a title.
+        const title = this.label ?? '';
+        switch (headingLevel) {
+            case 1:
+                return html `<h1 title="${title}">${content}</h1>`;
+            case 2:
+                return html `<h2 title="${title}">${content}</h2>`;
+            case 3:
+                return html `<h3 title="${title}">${content}</h3>`;
+            default:
+                return html `<h4 title="${title}">${content}</h4>`;
+        }
+    }
+    render() {
+        return html `<section
+      class="${e$3({
+            secondary: this.secondary,
+            highlighted: this.highlighted,
+            contrasted: this.resolvedLevel % 2 === 0,
+        })}"
+    >
+      ${this.renderHeader()}
+      <div><slot></slot></div>
+    </section>`;
+    }
+}
+OscdActionPane.scopedElements = {
+    'oscd-icon': OscdIcon,
+};
+OscdActionPane.styles = i$2 `
+    :host {
+      outline: none;
+    }
+
+    :host(:focus-within) section {
+      /* TODO consider using oscd-elevation instead */
+      box-shadow:
+        0 8px 10px 1px rgba(0, 0, 0, 0.14),
+        0 3px 14px 2px rgba(0, 0, 0, 0.12),
+        0 5px 5px -3px rgba(0, 0, 0, 0.2);
+      outline-width: 1px;
+      transition: all 250ms linear;
+    }
+
+    section {
+      background-color: var(
+        --oscd-action-pane-theme-surface,
+        var(--md-sys-color-surface)
+      );
+      transition: all 200ms linear;
+      outline-style: solid;
+      margin: 0px;
+      outline-width: 0px;
+      outline-color: var(
+        --oscd-action-pane-theme-primary,
+        var(--md-sys-color-primary)
+      );
+    }
+
+    section.secondary {
+      outline-color: var(
+        --oscd-action-pane-theme-secondary,
+        var(--md-sys-color-secondary)
+      );
+    }
+
+    section > div {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+      padding: 8px 12px 16px;
+      clear: right;
+    }
+
+    .highlighted {
+      outline-style: dotted;
+      outline-width: 2px;
+    }
+
+    :host(:focus-within) .highlighted {
+      outline-style: solid;
+    }
+
+    .contrasted {
+      background-color: var(
+        --oscd-action-pane-theme-on-primary,
+        var(--oscd-base2)
+      );
+    }
+
+    h1,
+    h2,
+    h3,
+    h4 {
+      color: var(
+        --oscd-action-pane-theme-on-surface,
+        var(--md-sys-color-on-surface)
+      );
+      font-family: var(--oscd-action-pane-theme-font, var(--oscd-text-font));
+      font-weight: 300;
+      overflow: clip visible;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      margin: 0px;
+      line-height: 52px;
+      padding-left: 0.3em;
+    }
+
+    nav {
+      float: right;
+      margin-right: 4px;
+    }
+
+    oscd-icon,
+    ::slotted([slot='icon']) {
+      vertical-align: middle;
+      position: relative;
+      top: -0.1em;
+    }
+  `;
+__decorate([
+    n$2({ type: String })
+], OscdActionPane.prototype, "label", void 0);
+__decorate([
+    n$2({ type: String })
+], OscdActionPane.prototype, "icon", void 0);
+__decorate([
+    n$2({ type: Boolean })
+], OscdActionPane.prototype, "secondary", void 0);
+__decorate([
+    n$2({ type: Boolean })
+], OscdActionPane.prototype, "highlighted", void 0);
+__decorate([
+    n$2({ type: Number })
+], OscdActionPane.prototype, "level", void 0);
+
+window.customElements.define('oscd-action-pane', OscdActionPane);
+
+export { OscdActionPane };
