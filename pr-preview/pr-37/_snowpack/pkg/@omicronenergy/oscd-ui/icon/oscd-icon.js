422773b16497ec7217347c88250fbdcbb6a9751a
clepski <clepski@users.noreply.github.com>
2026-04-01 12:25:14 +0000

    Deploy preview for PR 37 🛫

---
+++ pr-preview/pr-37/_snowpack/pkg/@omicronenergy/oscd-ui/icon/oscd-icon.js
@@ -0,0 +1,12 @@
+import { O as OscdIcon } from '../../../common/OscdIcon-753f0e38.js';
+export { O as OscdIcon } from '../../../common/OscdIcon-753f0e38.js';
+import '../../../common/lit-element-7082a332.js';
+import '../../../common/lit-html-487c1dfc.js';
+import '../../../common/render-d4c46927.js';
+
+/*
+ * GENERATED SOURCE FILE. DO NOT MODIFY.
+ * Modifications will be overwritten.
+ * To prevent this file from being overwritten, remove this comment entirely.
+ */
+customElements.define('oscd-icon', OscdIcon);
---
+++ pr-preview/pr-37/plugins/src/editors/protocol104/connectedap-editor.js
@@ -0,0 +1,61 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __decorate = (decorators, target, key, kind) => {
+  var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc(target, key) : target;
+  for (var i = decorators.length - 1, decorator; i >= 0; i--)
+    if (decorator = decorators[i])
+      result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+  if (kind && result)
+    __defProp(target, key, result);
+  return result;
+};
+import {customElement, html, property} from "../../../../_snowpack/pkg/lit-element.js";
+import "../../../../_snowpack/pkg/@omicronenergy/oscd-ui/fab/oscd-fab.js";
+import "../../../../_snowpack/link/packages/openscd/dist/action-icon.js";
+import {newWizardEvent} from "../../../../_snowpack/link/packages/openscd/dist/foundation.js";
+import {newActionEvent} from "../../../../_snowpack/link/packages/core/dist/index.js";
+import {editConnectedApWizard} from "./wizards/connectedap.js";
+import {Base104Container} from "./base-container.js";
+export let ConnectedAP104Editor = class extends Base104Container {
+  openEditWizard() {
+    this.dispatchEvent(newWizardEvent(() => editConnectedApWizard(this.element, this.element.querySelectorAll('Address > P[type^="RG"]').length > 0)));
+  }
+  remove() {
+    if (this.element)
+      this.dispatchEvent(newActionEvent({
+        old: {
+          parent: this.element.parentElement,
+          element: this.element,
+          reference: this.element.nextSibling
+        }
+      }));
+  }
+  render() {
+    return html`
+      <action-icon
+        label="${this.element.getAttribute("apName") ?? "UNDEFINED"}"
+        icon="settings_input_hdmi"
+        ><oscd-fab
+          slot="action"
+          mini
+          @click="${() => this.openEditWizard()}"
+        >
+          <oscd-icon slot="icon">edit</oscd-icon>
+        </oscd-fab>
+        <oscd-fab
+          slot="action"
+          mini
+          @click="${() => this.remove()}}"
+        >
+          <oscd-icon slot="icon">delete</oscd-icon>
+        </oscd-fab
+      ></action-icon>
+    `;
+  }
+};
+__decorate([
+  property({attribute: false})
+], ConnectedAP104Editor.prototype, "element", 2);
+ConnectedAP104Editor = __decorate([
+  customElement("connectedap-104-editor")
+], ConnectedAP104Editor);
