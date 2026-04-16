7abebf0ad1eb251c1475e05fccaf6c44694489a0
clepski <clepski@users.noreply.github.com>
2026-04-16 13:12:48 +0000

    Remove preview for PR 50 🛬

--- pr-preview/pr-50/_snowpack/pkg/lit.js
+++
@@ -1,3 +0,0 @@
-export { s as LitElement, i as css } from './common/lit-element-7082a332.js';
-export { h as html, n as nothing } from './common/lit-html-487c1dfc.js';
-import './common/render-d4c46927.js';
