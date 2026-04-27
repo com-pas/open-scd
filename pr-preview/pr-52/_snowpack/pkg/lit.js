aed314bd2e4e3666e40cc069ff60183541770e86
clepski <clepski@users.noreply.github.com>
2026-04-27 11:34:45 +0000

    Deploy preview for PR 52 🛫

---
+++ pr-preview/pr-52/_snowpack/pkg/lit.js
@@ -0,0 +1,3 @@
+export { s as LitElement, i as css } from './common/lit-element-7082a332.js';
+export { h as html, n as nothing } from './common/lit-html-487c1dfc.js';
+import './common/render-d4c46927.js';
