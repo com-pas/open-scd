83024a33f5610f507e71e064ad62d1d8cfb4b4b8
clepski <clepski@users.noreply.github.com>
2026-05-26 13:30:28 +0000

    Remove preview for PR 80 🛬

--- pr-preview/pr-80/_snowpack/pkg/lit-html.js
+++
@@ -1,2 +0,0 @@
-export { d as directive, h as html, n as nothing } from './common/lit-html-4eb216a4.js';
-export { r as render } from './common/render-4cc4e0f1.js';
--- pr-preview/pr-80/_snowpack/pkg/tslib.js
+++
@@ -1,1 +0,0 @@
-export { _ as __decorate } from './common/tslib.es6-98fe39c2.js';
