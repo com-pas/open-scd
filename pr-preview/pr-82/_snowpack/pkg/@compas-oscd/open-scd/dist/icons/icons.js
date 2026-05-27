0d18ce59abcd9877b13380b942d43efe2cf3d856
clepski <clepski@users.noreply.github.com>
2026-05-27 10:14:54 +0000

    Remove preview for PR 82 🛬

--- pr-preview/pr-82/_snowpack/pkg/@compas-oscd/open-scd/dist/icons/icons.js
+++
@@ -1,4 +0,0 @@
-export { k as clientIcon, w as connectivityNodeIcon, m as controlBlockIcons, r as dataTypeTemplateIcons, x as editIcon, g as generalConductingEquipmentIcon, q as getFilterIcon, j as getIcon, n as inputIcon, o as openSCDIcon, y as powerTransformerTwoWindingIcon, t as sizableGooseIcon, u as sizableSmvIcon, z as transformerWindingIcon } from '../../../../common/icons-5878fb5c.js';
-import '../../../../common/lit-element-5461ae81.js';
-import '../../../../common/lit-html-4eb216a4.js';
-import '../../../../common/render-4cc4e0f1.js';
