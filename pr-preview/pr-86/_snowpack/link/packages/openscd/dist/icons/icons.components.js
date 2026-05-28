a3db52803f3d734cc027d7daa06992f68c3d3b0d
noraeb <noraeb@users.noreply.github.com>
2026-05-28 08:04:03 +0000

    Deploy preview for PR 86 🛫

---
+++ pr-preview/pr-86/_snowpack/link/packages/openscd/dist/icons/icons.components.js
@@ -0,0 +1,266 @@
+import { __decorate } from "../../../../../pkg/tslib.js";
+import { customElement, html, LitElement, } from '../../../../../pkg/lit-element.js';
+import { bayIcon, circuitBreakerIcon, currentTransformerIcon, disconnectorIcon, earthSwitchIcon, generalConductingEquipmentIcon, gooseIcon, lineIcon, processIcon, smvIcon, substationIcon, voltageLevelIcon, voltageTransformerIcon } from './icons.js';
+import { automationLogicalNode, controlLogicalNode, functionalLogicalNode, furtherPowerSystemEquipmentLogicalNode, generalLogicalNode, interfacingLogicalNode, measurementLogicalNode, nonElectricalLogicalNode, powerTransformerLogicalNode, protectionLogicalNode, protectionRelatedLogicalNode, qualityLogicalNode, supervisionLogicalNode, switchgearLogicalNode, systemLogicalNode, transformerLogicalNode, } from './lnode.js';
+let CustomIconBay = class CustomIconBay extends LitElement {
+    render() {
+        return html `${bayIcon}`;
+    }
+};
+CustomIconBay = __decorate([
+    customElement('custom-icon-bay')
+], CustomIconBay);
+export { CustomIconBay };
+let CustomIconSubstation = class CustomIconSubstation extends LitElement {
+    render() {
+        return html `${substationIcon}`;
+    }
+};
+CustomIconSubstation = __decorate([
+    customElement('custom-icon-substation')
+], CustomIconSubstation);
+export { CustomIconSubstation };
+let CustomIconVoltageLevel = class CustomIconVoltageLevel extends LitElement {
+    render() {
+        return html `${voltageLevelIcon}`;
+    }
+};
+CustomIconVoltageLevel = __decorate([
+    customElement('custom-icon-voltagelevel')
+], CustomIconVoltageLevel);
+export { CustomIconVoltageLevel };
+let CustomIconCircuitBreaker = class CustomIconCircuitBreaker extends LitElement {
+    render() {
+        return html `${circuitBreakerIcon}`;
+    }
+};
+CustomIconCircuitBreaker = __decorate([
+    customElement('custom-icon-circuitbreaker')
+], CustomIconCircuitBreaker);
+export { CustomIconCircuitBreaker };
+let CustomIconDisconnector = class CustomIconDisconnector extends LitElement {
+    render() {
+        return html `${disconnectorIcon}`;
+    }
+};
+CustomIconDisconnector = __decorate([
+    customElement('custom-icon-disconnector')
+], CustomIconDisconnector);
+export { CustomIconDisconnector };
+let CustomIconCurrentTransformer = class CustomIconCurrentTransformer extends LitElement {
+    render() {
+        return html `${currentTransformerIcon}`;
+    }
+};
+CustomIconCurrentTransformer = __decorate([
+    customElement('custom-icon-currenttransformer')
+], CustomIconCurrentTransformer);
+export { CustomIconCurrentTransformer };
+let CustomIconVoltageTransformer = class CustomIconVoltageTransformer extends LitElement {
+    render() {
+        return html `${voltageTransformerIcon}`;
+    }
+};
+CustomIconVoltageTransformer = __decorate([
+    customElement('custom-icon-voltagetransformer')
+], CustomIconVoltageTransformer);
+export { CustomIconVoltageTransformer };
+let CustomIconEarthSwitch = class CustomIconEarthSwitch extends LitElement {
+    render() {
+        return html `${earthSwitchIcon}`;
+    }
+};
+CustomIconEarthSwitch = __decorate([
+    customElement('custom-icon-earthswitch')
+], CustomIconEarthSwitch);
+export { CustomIconEarthSwitch };
+let CustomIconGeneralConductingEquipment = class CustomIconGeneralConductingEquipment extends LitElement {
+    render() {
+        return html `${generalConductingEquipmentIcon}`;
+    }
+};
+CustomIconGeneralConductingEquipment = __decorate([
+    customElement('custom-icon-generalconductingequipment')
+], CustomIconGeneralConductingEquipment);
+export { CustomIconGeneralConductingEquipment };
+let CustomIconGoose = class CustomIconGoose extends LitElement {
+    render() {
+        return html `${gooseIcon}`;
+    }
+};
+CustomIconGoose = __decorate([
+    customElement('custom-icon-goose')
+], CustomIconGoose);
+export { CustomIconGoose };
+let CustomIconSmv = class CustomIconSmv extends LitElement {
+    render() {
+        return html `${smvIcon}`;
+    }
+};
+CustomIconSmv = __decorate([
+    customElement('custom-icon-smv')
+], CustomIconSmv);
+export { CustomIconSmv };
+let CustomIconLine = class CustomIconLine extends LitElement {
+    render() {
+        return html `${lineIcon}`;
+    }
+};
+CustomIconLine = __decorate([
+    customElement('custom-icon-line')
+], CustomIconLine);
+export { CustomIconLine };
+let CustomIconProcess = class CustomIconProcess extends LitElement {
+    render() {
+        return html `${processIcon}`;
+    }
+};
+CustomIconProcess = __decorate([
+    customElement('custom-icon-process')
+], CustomIconProcess);
+export { CustomIconProcess };
+let CustomIconLNodeAutomation = class CustomIconLNodeAutomation extends LitElement {
+    render() {
+        return html `${automationLogicalNode}`;
+    }
+};
+CustomIconLNodeAutomation = __decorate([
+    customElement('custom-icon-lnode-automation')
+], CustomIconLNodeAutomation);
+export { CustomIconLNodeAutomation };
+let CustomIconLNodeControl = class CustomIconLNodeControl extends LitElement {
+    render() {
+        return html `${controlLogicalNode}`;
+    }
+};
+CustomIconLNodeControl = __decorate([
+    customElement('custom-icon-lnode-control')
+], CustomIconLNodeControl);
+export { CustomIconLNodeControl };
+let CustomIconLNodeFunctional = class CustomIconLNodeFunctional extends LitElement {
+    render() {
+        return html `${functionalLogicalNode}`;
+    }
+};
+CustomIconLNodeFunctional = __decorate([
+    customElement('custom-icon-lnode-functional')
+], CustomIconLNodeFunctional);
+export { CustomIconLNodeFunctional };
+let CustomIconLNodeFurtherPowerSystemEquipment = class CustomIconLNodeFurtherPowerSystemEquipment extends LitElement {
+    render() {
+        return html `${furtherPowerSystemEquipmentLogicalNode}`;
+    }
+};
+CustomIconLNodeFurtherPowerSystemEquipment = __decorate([
+    customElement('custom-icon-lnode-furtherpowersystemequipment')
+], CustomIconLNodeFurtherPowerSystemEquipment);
+export { CustomIconLNodeFurtherPowerSystemEquipment };
+let CustomIconLNodeGeneral = class CustomIconLNodeGeneral extends LitElement {
+    render() {
+        return html `${generalLogicalNode}`;
+    }
+};
+CustomIconLNodeGeneral = __decorate([
+    customElement('custom-icon-lnode-general')
+], CustomIconLNodeGeneral);
+export { CustomIconLNodeGeneral };
+let CustomIconLNodeInterfacing = class CustomIconLNodeInterfacing extends LitElement {
+    render() {
+        return html `${interfacingLogicalNode}`;
+    }
+};
+CustomIconLNodeInterfacing = __decorate([
+    customElement('custom-icon-lnode-interfacing')
+], CustomIconLNodeInterfacing);
+export { CustomIconLNodeInterfacing };
+let CustomIconLNodeMeasurement = class CustomIconLNodeMeasurement extends LitElement {
+    render() {
+        return html `${measurementLogicalNode}`;
+    }
+};
+CustomIconLNodeMeasurement = __decorate([
+    customElement('custom-icon-lnode-measurement')
+], CustomIconLNodeMeasurement);
+export { CustomIconLNodeMeasurement };
+let CustomIconLNodeNonElectrical = class CustomIconLNodeNonElectrical extends LitElement {
+    render() {
+        return html `${nonElectricalLogicalNode}`;
+    }
+};
+CustomIconLNodeNonElectrical = __decorate([
+    customElement('custom-icon-lnode-nonelectrical')
+], CustomIconLNodeNonElectrical);
+export { CustomIconLNodeNonElectrical };
+let CustomIconLNodePowerTransformer = class CustomIconLNodePowerTransformer extends LitElement {
+    render() {
+        return html `${powerTransformerLogicalNode}`;
+    }
+};
+CustomIconLNodePowerTransformer = __decorate([
+    customElement('custom-icon-lnode-powertransformer')
+], CustomIconLNodePowerTransformer);
+export { CustomIconLNodePowerTransformer };
+let CustomIconLNodeProtection = class CustomIconLNodeProtection extends LitElement {
+    render() {
+        return html `${protectionLogicalNode}`;
+    }
+};
+CustomIconLNodeProtection = __decorate([
+    customElement('custom-icon-lnode-protection')
+], CustomIconLNodeProtection);
+export { CustomIconLNodeProtection };
+let CustomIconLNodeProtectionRelated = class CustomIconLNodeProtectionRelated extends LitElement {
+    render() {
+        return html `${protectionRelatedLogicalNode}`;
+    }
+};
+CustomIconLNodeProtectionRelated = __decorate([
+    customElement('custom-icon-lnode-protectionrelated')
+], CustomIconLNodeProtectionRelated);
+export { CustomIconLNodeProtectionRelated };
+let CustomIconLNodeQuality = class CustomIconLNodeQuality extends LitElement {
+    render() {
+        return html `${qualityLogicalNode}`;
+    }
+};
+CustomIconLNodeQuality = __decorate([
+    customElement('custom-icon-lnode-quality')
+], CustomIconLNodeQuality);
+export { CustomIconLNodeQuality };
+let CustomIconLNodeSupervision = class CustomIconLNodeSupervision extends LitElement {
+    render() {
+        return html `${supervisionLogicalNode}`;
+    }
+};
+CustomIconLNodeSupervision = __decorate([
+    customElement('custom-icon-lnode-supervision')
+], CustomIconLNodeSupervision);
+export { CustomIconLNodeSupervision };
+let CustomIconLNodeSwitchgear = class CustomIconLNodeSwitchgear extends LitElement {
+    render() {
+        return html `${switchgearLogicalNode}`;
+    }
+};
+CustomIconLNodeSwitchgear = __decorate([
+    customElement('custom-icon-lnode-switchgear')
+], CustomIconLNodeSwitchgear);
+export { CustomIconLNodeSwitchgear };
+let CustomIconLNodeSystem = class CustomIconLNodeSystem extends LitElement {
+    render() {
+        return html `${systemLogicalNode}`;
+    }
+};
+CustomIconLNodeSystem = __decorate([
+    customElement('custom-icon-lnode-system')
+], CustomIconLNodeSystem);
+export { CustomIconLNodeSystem };
+let CustomIconLNodeTransformer = class CustomIconLNodeTransformer extends LitElement {
+    render() {
+        return html `${transformerLogicalNode}`;
+    }
+};
+CustomIconLNodeTransformer = __decorate([
+    customElement('custom-icon-lnode-transformer')
+], CustomIconLNodeTransformer);
+export { CustomIconLNodeTransformer };
+//# sourceMappingURL=icons.components.js.map
